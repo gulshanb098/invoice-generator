7edacf7dd8739903c01420093cb118da8af8481c
Gulshan Baraik <gulshan.baraik@goavega.com>
2024-09-14 23:11:12 +0530

    login endpoint added

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { User } from "../models/user-model";
 
 export const registration = async (req: Request, res: Response) => {
@@ -22,4 +23,23 @@ export const registration = async (req: Request, res: Response) => {
       password,
     },
   });
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: "Invalid Credentials" });
+  }
+
+  const userToken = jwt.sign(
+    { userId: user._id },
+    process.env.JWT_SECRET as string
+  );
+  res.status(200).json({ token: userToken });
 };
--- src/routes/auth-route.ts
+++ src/routes/auth-route.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { registration } from "../controllers/auth-controller";
+import { login, registration } from "../controllers/auth-controller";
 const router = express.Router();
 
 router.post("/registration", registration);
+router.post("/login", login);
 
 export default router;
